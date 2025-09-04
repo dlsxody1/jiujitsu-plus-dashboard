665e302e1bf136ca7557fa43ec280d7b5c10942e
dlsxody1 <dlsxody1@naver.com>
2025-09-04 15:12:21 +0900

    chore : shadcn/ui 컴포넌트 세팅 및 설치

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,13 @@
 
 import { tanstackConfig } from '@tanstack/eslint-config'
 
-export default [...tanstackConfig]
+export default [
+  ...tanstackConfig,
+  {
+    rules: {
+      'import/order': 'off',
+      'sort-imports': 'off',
+      '@typescript-eslint/array-type': 'off',
+    },
+  },
+]
